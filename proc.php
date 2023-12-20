29c704319a31a499b2af231f6b17a451ad1a7c78
gsteam202 <gsteam@naver.com>
2023-12-20 12:42:20 +0900

    ㅇ

--- proc.php
+++ proc.php
@@ -3,5 +3,6 @@ if (empty($_GET['className'])) {
     exit;
 }
 ?>
+<script type="text/javascript" src="talents/<?php echo $_GET['className']; ?>/variables.js"></script>
 <script type="text/javascript" src="talents/<?php echo $_GET['className']; ?>/data.js"></script>
 <script type="text/javascript" src="talents/common.js"></script>
