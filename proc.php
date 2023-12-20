02209622b4dc889db6e2793b161b141f32ec77ca
gsteam202 <gsteam@naver.com>
2023-12-20 12:43:05 +0900

    ㅇ

--- proc.php
+++ proc.php
@@ -4,5 +4,6 @@ if (empty($_GET['className'])) {
 }
 ?>
 <script type="text/javascript" src="talents/<?php echo $_GET['className']; ?>/variables.js"></script>
+<script type="text/javascript" src="talents/<?php echo $_GET['className']; ?>/functions.js"></script>
 <script type="text/javascript" src="talents/<?php echo $_GET['className']; ?>/data.js"></script>
 <script type="text/javascript" src="talents/common.js"></script>
